971de2eb3ce80e819ac9faca167122d75d87b3c8
fengzhen1980 <tj.fengzhen@hotmail.com>
2023-05-13 02:00:27 -0400

    2023-05-13 add LoginCheckFilter

---
+++ reggie_take_out/src/main/java/com/CapstoneProject/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,21 @@
+package com.CapstoneProject.reggie.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        log.info("*----> Intercept request: {}", request.getRequestURI());
+        filterChain.doFilter(request,response);
+    }
+}
