450f6642681caa0e94202f2eee4ce4837ca0ada7
fengzhen1980 <tj.fengzhen@hotmail.com>
2023-05-14 01:13:07 -0400

    2023-05-14 add LoginCheckFilter

--- reggie_take_out/src/main/java/com/CapstoneProject/reggie/filter/LoginCheckFilter.java
+++ reggie_take_out/src/main/java/com/CapstoneProject/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,9 @@
 package com.CapstoneProject.reggie.filter;
 
+import com.CapstoneProject.reggie.common.R;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -11,11 +14,59 @@ import java.io.IOException;
 @WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
 @Slf4j
 public class LoginCheckFilter implements Filter {
+
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest)servletRequest;
         HttpServletResponse response = (HttpServletResponse)servletResponse;
-        log.info("*----> Intercept request: {}", request.getRequestURI());
-        filterChain.doFilter(request,response);
+
+        String requestURI = request.getRequestURI();
+        log.info("*----> intercept request: {}", requestURI);
+
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        boolean check = check(urls,requestURI);
+
+        // if the URI cna be released, don't do else.
+        if(check) {
+            log.info("*----> release request: {}", requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        // if there is employee information in Session, it can be released.
+        if(request.getSession().getAttribute("employee") != null) {
+            log.info("*----> User logged in. User id:{}", request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        // not login
+        log.info("*----> User didn't login.");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+
+    /**
+     * Check whether this request can be released?
+     * @param urls
+     * @param requestURI
+     * @return
+     */
+    public boolean check(String[] urls, String requestURI) {
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if(match) {
+                return true;
+            }
+        }
+        return false;
     }
 }
