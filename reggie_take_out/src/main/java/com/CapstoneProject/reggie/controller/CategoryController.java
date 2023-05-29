3c20890eeea17649f3fc1a7ba2587b75c2f7eb0b
fengzhen1980 <tj.fengzhen@hotmail.com>
2023-05-29 14:12:53 -0400

    2023-05-29 Implement category list

--- reggie_take_out/src/main/java/com/CapstoneProject/reggie/controller/CategoryController.java
+++ reggie_take_out/src/main/java/com/CapstoneProject/reggie/controller/CategoryController.java
@@ -4,12 +4,11 @@ import com.CapstoneProject.reggie.common.R;
 import com.CapstoneProject.reggie.entity.Category;
 import com.CapstoneProject.reggie.service.CategoryService;
 import com.CapstoneProject.reggie.service.impl.CategoryServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -29,5 +28,18 @@ public class CategoryController {
         log.info("category:{}", category);
         categoryService.save(category);
         return R.success("Create New Category Successfully");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+
+        Page<Category> pageInfo = new Page<>();
+
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category :: getSort);
+
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
     }
 }
