01d1d07e6d9b97a940b96c4ddd272856d6803277
fengzhen1980 <tj.fengzhen@hotmail.com>
2023-06-09 23:19:15 -0400

    2023-06-09 delete setmeal information

--- reggie_take_out/src/main/java/com/CapstoneProject/reggie/controller/SetmealController.java
+++ reggie_take_out/src/main/java/com/CapstoneProject/reggie/controller/SetmealController.java
@@ -1,11 +1,21 @@
 package com.CapstoneProject.reggie.controller;
 
+import com.CapstoneProject.reggie.common.R;
+import com.CapstoneProject.reggie.dto.SetmealDto;
+import com.CapstoneProject.reggie.entity.Category;
+import com.CapstoneProject.reggie.entity.Setmeal;
+import com.CapstoneProject.reggie.service.CategoryService;
 import com.CapstoneProject.reggie.service.SetmealDishService;
 import com.CapstoneProject.reggie.service.SetmealService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/setmeal")
@@ -16,5 +26,58 @@ public class SetmealController {
     private SetmealService setmealService;
 
     @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
     private SetmealDishService setmealDishService;
+
+    @PostMapping
+    public R<String> save(@RequestBody SetmealDto setmealDto) {
+
+        log.info("*----> setmeal:{}", setmealDto);
+
+        setmealService.saveWithDish(setmealDto);
+
+        return R.success("Add set meal successful.");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);
+        Page<SetmealDto> dtoPage = new Page<>(page, pageSize);
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(name != null, Setmeal::getName, name);
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        // according to queryWrapper, query result. put the result into pageInfo
+        setmealService.page(pageInfo, queryWrapper);
+
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+        List<Setmeal> records = pageInfo.getRecords();
+        List<SetmealDto> list = records.stream().map((item) ->{
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item, setmealDto);
+
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            if(category != null){
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(list);
+
+        return R.success(dtoPage);
+    }
+
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids){
+
+        setmealService.removeWithDish(ids);
+        return R.success("Delete successful");
+    }
 }
--- reggie_take_out/src/main/java/com/CapstoneProject/reggie/service/SetmealService.java
+++ reggie_take_out/src/main/java/com/CapstoneProject/reggie/service/SetmealService.java
@@ -1,7 +1,14 @@
 package com.CapstoneProject.reggie.service;
 
+import com.CapstoneProject.reggie.dto.SetmealDto;
 import com.CapstoneProject.reggie.entity.Setmeal;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 public interface SetmealService extends IService<Setmeal> {
+
+    public void saveWithDish(SetmealDto setmealDto);
+
+    public void removeWithDish(List<Long> ids);
 }
